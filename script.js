52b28dd0bc5c8b70e37e8b9e959d833575843db5
Shaun <sandstrom.shaun@gmail.com>
2022-01-20 10:20:42 -0700

    have operators in print statements working

--- script.js
+++ script.js
@@ -7,32 +7,74 @@ function submit() {
        if(text[5] && text[5] === '(') {
          stack.push(text[5]);
        };
-       let i = 7;
        let output = "";
-       if(text[6] && text[6] === '"') {
-         while(text[i] && text[i] !== '"') {
-           output += text[i];
+       let isNumberRegexp = new RegExp("[0-9]")
+       if(text[6] && isNumberRegexp.test(text[6])) {
+         let num1 = text[6];
+         let i = 7;
+         while(text[i] && isNumberRegexp.test(text[i]) && text[i] !== ' ') {
+           num1 += text[i];
            i++;
          }
+         let num2 = "";
+         let operator = "";
+         if(text[i] && text[i] === ' ') {
+           i++;
+           if(text[i] === '>' || text[i] === '<') {
+             operator = text[i];
+             i++;
+             if(text[i] && text[i] === ' ') {
+               i++;
+               while(text[i] && isNumberRegexp.test(text[i])) {
+                 num2 += text[i];
+                 i++;
+               }
+             }
+           }
+         }
+         let num1AsNum = Number(num1);
+         let num2AsNum = Number(num2);
+         if(operator === '<') {
+           if(num1AsNum < num2AsNum) {
+             document.getElementById("output").innerHTML = "True";
+           } else {
+             document.getElementById("output").innerHTML = "False";
+           }
+         } else if(operator === '>') {
+           if(num1AsNum > num2AsNum) {
+             document.getElementById("output").innerHTML = "True";
+           } else {
+             document.getElementById("output").innerHTML = "False";
+           }
+         }
+       } else {
+         let i = 7;
+         if(text[6] && text[6] === '"') {
+           while(text[i] && text[i] !== '"') {
+             output += text[i];
+             i++;
+           }
+         }
+         i++;
+         if(text[i] && text[i] === ')') {
+           document.getElementById("output").innerHTML = output;
+         }
        }
-       i++;
-       if(text[i] && text[i] === ')') {
-         document.getElementById("output").innerHTML = output;
-       }
-  }
-  let firstCharacterOfVarRegexp = new RegExp("[a-z]", "i");
-  let restOfVarRegexp = new RegExp("[a-z0-9_]", "i");
+  } else {
+    let firstCharacterOfVarRegexp = new RegExp("[a-z]", "i");
+    let restOfVarRegexp = new RegExp("[a-z0-9_]", "i");
 
-  if(text[0] && firstCharacterOfVarRegexp.test(text[0])) {
-    let varName = text[0];
-    let i = 1;
-    while(text[i] && text[i] !== ' ') {
-      if(!restOfVarRegexp.test(text[i])) {
-        document.getElementById("output").innerHTML = "Invalid variable name";
-        break;
+    if(text[0] && firstCharacterOfVarRegexp.test(text[0])) {
+      let varName = text[0];
+      let i = 1;
+      while(text[i] && text[i] !== ' ') {
+        if(!restOfVarRegexp.test(text[i])) {
+          document.getElementById("output").innerHTML = "Invalid variable name";
+          break;
+        }
+        varName += text[i];
+        i++;
       }
-      varName += text[i];
-      i++;
     }
   }
 }
