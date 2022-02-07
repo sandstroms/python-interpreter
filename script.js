6cd52dfc28228af94323eeef25606d850cfef1e1
Shaun <sandstrom.shaun@gmail.com>
2022-02-06 20:47:18 -0700

    json dumps is working

--- script.js
+++ script.js
@@ -165,7 +165,55 @@ function submit() {
              }
            }
       }
-      if(text[i] && text[i] === "\"" || text[i] === "'") {
+      if(text[i] && text[i] === 'j' && text[i+1] && text[i+1] === 's' &&
+         text[i+2] && text[i+2] === 'o' && text[i+3] && text[i+3] === 'n' &&
+         text[i+4] && text[i+4] === '.' && text[i+5] && text[i+5] === 'd' &&
+         text[i+6] && text[i+6] === "u" && text[i+7] && text[i+7] === "m" &&
+         text[i+8] && text[i+8] === "p" && text[i+9] && text[i+9] === "s") {
+        let pythonDict = variableArr[1];
+        let keys = Object.keys(pythonDict);
+        let jsonString = "{";
+        jsonString += "\"";
+        jsonString += keys[0];
+        jsonString += "\": \"";
+        jsonString += pythonDict[keys[0]];
+        jsonString += "\", \"";
+        jsonString += keys[1];
+        jsonString += "\": \""
+        jsonString += pythonDict[keys[1]];
+        jsonString += "\", \"";
+        jsonString += keys[2];
+        jsonString += "\": \""
+        jsonString += pythonDict[keys[2]];
+        jsonString += "\"}";
+        variableObject[varName] = jsonString;
+        variableType[varName] = "str";
+      }
+      if(text[i] && text[i] === "{") {
+        let pythonDict = {};
+        i++;
+        while(text[i] && text[i] !== "\"") {
+          i++;
+        }
+        i++;
+        let key;
+        [key, i] = parseKey(text, i);
+        i += 3;
+        let value;
+        [value, i] = parseValue(text, i);
+        pythonDict[key] = value;
+        i += 2;
+        [key, i] = parseKey(text, i);
+        i += 3;
+        [value, i] = parseValue(text, i);
+        pythonDict[key] = value;
+        i += 2;
+        [key, i] = parseKey(text, i);
+        i += 3;
+        [value, i] = parseValue(text, i);
+        pythonDict[key] = value;
+        variableArr[1] = pythonDict;
+      } else if(text[i] && text[i] === "\"" || text[i] === "'") {
         let quote = text[i];
         i++;
         while(text[i] && text[i] !== "\"" && text[i] !== "'") {
@@ -192,7 +240,7 @@ function submit() {
         }
         variableType[varName] = 'list';
         variableObject[varName] = list;
-      } else {
+      } else if(text[i] && isNumberRegexp.test(text[i])) {
         let isFloat = false;
         while(text[i] && isNumberRegexp.test(text[i])) {
           variableValue += text[i];
@@ -214,6 +262,39 @@ function submit() {
     } else if(text[0] === '#') {
       // do nothing for a comment
     }
+}
+
+function parseKey(text, i) {
+  let key = "";
+  if(text[i] === "\"") {
+    i++;
+  }
+  while(text[i] && text[i] !== "\"") {
+    key += text[i];
+    i++;
+  }
+  return [key, i]
+}
+
+function parseValue(text, i) {
+  let value = "";
+  let numberRegex = new RegExp("[0-9]");
+  if(text[i] === "\"") {
+    i++;
+    while(text[i] && text[i] !== "\"") {
+      value += text[i];
+      i++;
+    }
+    i++;
+  } else if(numberRegex.test(text[i])) {
+    value += text[i];
+    while(numberRegex.test(text[i])) {
+      value += text[i];
+      i++;
+    }
+    value = Number(value);
+  }
+  return [value, i];
 }
 
 function parseAFloat(int) {
